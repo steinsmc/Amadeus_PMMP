7b364456631a776e3c5abf346ef0e9a5a4e94844
William Wang <dhdjwjy@gmail.com>
2019-04-29 12:35:20 -0400

    整体格式化

--- src/PMMP/Server/PM.php
+++ src/PMMP/Server/PM.php
@@ -25,15 +25,15 @@ class PM
     {
         if (!file_exists($this->Directory . '/bin/php7/bin/php')) {
             system('tar -zxvf ' . Process::getCache() . '/php7.2-linux.tar.gz -C ' . $this->Directory . '/ 2>&1', $ret);
-            if($ret!=0){
-                Logger::printLine('Failed to decompress php@7.2 library',Logger::LOG_FATAL);
+            if ($ret != 0) {
+                Logger::printLine('Failed to decompress php@7.2 library', Logger::LOG_FATAL);
                 return false;
             }
         }
         if (!file_exists($this->Directory . '/Pocketmine-MP.phar')) {
-            system('cp -r '.Process::getCache().'/Pocketmine-MP.phar ' . $this->Directory . '/Pocketmine-MP.phar 2>&1', $ret);
-            if($ret!=0){
-                Logger::printLine('Failed to copy Pocketmine-MP@latest',Logger::LOG_FATAL);
+            system('cp -r ' . Process::getCache() . '/Pocketmine-MP.phar ' . $this->Directory . '/Pocketmine-MP.phar 2>&1', $ret);
+            if ($ret != 0) {
+                Logger::printLine('Failed to copy Pocketmine-MP@latest', Logger::LOG_FATAL);
                 return false;
             }
         }
