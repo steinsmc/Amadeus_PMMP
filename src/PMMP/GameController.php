65fd26eb6585c25da80a6fa87c93e33068c82c15
William Wang <dhdjwjy@gmail.com>
2019-04-28 20:52:08 -0400

    关于服务器init的更新

--- src/PMMP/GameController.php
+++ src/PMMP/GameController.php
@@ -23,6 +23,32 @@ class GameController extends \Amadeus\Plugin\Game\GameController implements Game
     public function onLoading()
     {
         Logger::printLine('Pocketmine-MP support for Amadeus is loading');
+        if (!file_exists(Process::getCache() . '/php7.2-linux.tar.gz')) {
+            file_put_contents(Process::getCache() . '/php.sh', file_get_contents('https://raw.githubusercontent.com/pmmp/php-build-scripts/master/compile.sh'));
+            Logger::printLine('Building php@7.2 library', Logger::LOG_INFORM);
+            system('cd ' . Process::getCache() . ' && sh php.sh -t linux64 -l -g -u -j4 -f x86_64 >> ' . Process::getBase() . '/Amadeus.log 2>&1', $ret);
+            if ($ret != 0) {
+                Logger::printLine('Failed to build php@7.2 library', Logger::LOG_FATAL);
+                return false;
+            }
+            system('cd ' . Process::getCache() . ' && tar -zcvf php7.2-linux.tar.gz ./bin', $ret);
+            if ($ret != 0) {
+                Logger::printLine('Failed to compress php@7.2 library', Logger::LOG_FATAL);
+                return false;
+            }
+            //system('cd '.Process::getCache().' && rm -rf bin php.sh');
+            Logger::printLine('Successfully built php@7.2 library', Logger::LOG_SUCCESS);
+        }
+        Logger::printLine('Found php@7.2 library', Logger::LOG_INFORM);
+        if (!file_exists(Process::getCache() . '/Pocketmine-MP.phar')) {
+            Logger::printLine('Downloading Pocketmine-MP@latest', Logger::LOG_INFORM);
+            system('wget https://jenkins.pmmp.io/job/PocketMine-MP/lastSuccessfulBuild/artifact/PocketMine-MP.phar -O ' . Process::getCache() . '/Pocketmine-MP.phar >> ' . Process::getBase() . '/Amadeus.log 2>&1', $ret);
+            if ($ret != 0) {
+                Logger::printLine('Failed to download Pocketmine-MP@latest', Logger::LOG_FATAL);
+                return false;
+            }
+        }
+        Logger::printLine('Found Pocketmine-MP@latest', Logger::LOG_INFORM);
         return true;
     }
 
--- src/PMMP/Server/PM.php
+++ src/PMMP/Server/PM.php
@@ -5,6 +5,7 @@ namespace PMMP\Server;
 
 
 use Amadeus\IO\Logger;
+use Amadeus\Process;
 
 class PM
 {
@@ -20,15 +21,21 @@ class PM
         $this->PluginDirectory = $PluginDirectory;
     }
 
-    public function init()
+    public function init(): bool
     {
-        if(!file_exists($this->Directory.'/bin/php7/bin/php')){
-            system('tar -zxvf '.$this->PluginDirectory.'/res/php7.2-linux.tar.gz -C '.$this->Directory.'/ 2>&1',$ret);
-            Logger::printLine($ret,Logger::LOG_INFORM);
+        if (!file_exists($this->Directory . '/bin/php7/bin/php')) {
+            system('tar -zxvf ' . Process::getCache() . '/php7.2-linux.tar.gz -C ' . $this->Directory . '/ 2>&1', $ret);
+            if($ret!=0){
+                Logger::printLine('Failed to decompress php@7.2 library',Logger::LOG_FATAL);
+                return false;
+            }
         }
-        if(!file_exists($this->Directory.'/Pocketmine-MP.phar')){
-            system('wget https://jenkins.pmmp.io/job/PocketMine-MP/lastSuccessfulBuild/artifact/PocketMine-MP.phar -O '.$this->Directory.'/Pocketmine-MP.phar 2>&1',$ret);
-            Logger::printLine($ret,Logger::LOG_INFORM);
+        if (!file_exists($this->Directory . '/Pocketmine-MP.phar')) {
+            system('cp -r '.Process::getCache().'/Pocketmine-MP.phar ' . $this->Directory . '/Pocketmine-MP.phar 2>&1', $ret);
+            if($ret!=0){
+                Logger::printLine('Failed to copy Pocketmine-MP@latest',Logger::LOG_FATAL);
+                return false;
+            }
         }
         return true;
     }
