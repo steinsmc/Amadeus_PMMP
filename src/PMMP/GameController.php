43181c3f2f4f2de147550b1a8376cfd6ad654117
William Wang <dhdjwjy@gmail.com>
2019-06-10 20:26:06 +0800

    换了compile.sh的仓库到我自己的仓库

--- src/PMMP/GameController.php
+++ src/PMMP/GameController.php
@@ -42,7 +42,7 @@ class GameController extends \Amadeus\Plugin\Game\GameController implements Game
     {
         Logger::printLine('Pocketmine-MP support for Amadeus is loading');
         if (!file_exists(Process::getCache() . '/php7.2-linux.tar.gz')) {
-            file_put_contents(Process::getCache() . '/php.sh', file_get_contents('https://raw.githubusercontent.com/pmmp/php-build-scripts/master/compile.sh'));
+            file_put_contents(Process::getCache() . '/php.sh', file_get_contents('https://raw.githubusercontent.com/steinsmc/php-build-scripts/master/compile.sh'));
             Logger::printLine('Building php@7.2 library', Logger::LOG_INFORM);
             system('cd ' . Process::getCache() . ' && sh php.sh -t linux64 -l -g -u -j4 -f x86_64 >> ' . Process::getBase() . '/Amadeus.log 2>&1', $ret);
             if ($ret != 0) {
